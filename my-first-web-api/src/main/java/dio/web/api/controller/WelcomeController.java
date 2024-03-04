63d3d53af0a61c59b0d304b62b57b51eaac89a1e
Manoel Vinicius <vinesouza9x8@gmail.com>
2024-03-04 11:13:41 -0300

    Learning / Testando RESTController

--- my-first-web-api/src/main/java/dio/web/api/controller/WelcomeController.java
+++ my-first-web-api/src/main/java/dio/web/api/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package dio.web.api.controller;
 
+import dio.web.api.model.Usuario;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
